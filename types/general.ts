fa60c6eb512180d3880b8da2db355ec30c29ee28
Qreamville <Oyeleyebalikis@yahoo.com>
2023-09-28 14:26:04 +0100

    feat: create constant files

--- types/general.ts
+++ types/general.ts
@@ -8,3 +8,31 @@ export type SidebarProps = {
   setOpenSidebar: React.Dispatch<React.SetStateAction<boolean>>;
   openSidebar?: boolean;
 };
+
+export type ProductProps = {
+  id: number;
+  img: string;
+  name: string;
+  category: string;
+  price: number;
+  quantity: number;
+  status: string;
+};
+
+export type ProductsProps = {
+  products: ProductProps[];
+};
+
+export type OrderProps = {
+  orderId: string;
+  img: string;
+  name: string;
+  address: string;
+  date: string;
+  amount: number;
+  status: string;
+};
+
+export type OrdersProps = {
+  order: OrderProps[];
+};
