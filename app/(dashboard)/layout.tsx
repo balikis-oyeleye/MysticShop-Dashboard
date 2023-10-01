7db189d22fd7281ae9eb3f51e21bb592b3e91951
Qreamville <Oyeleyebalikis@yahoo.com>
2023-10-01 10:46:00 +0100

    style: change sidebar in mobile view

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,13 +11,13 @@ const DashboardLayout = ({ children }: LayoutProps) => {
   return (
     <div className="h-full relative">
       <aside
-        className={`h-full fixed inset-y-0 z-[80] bg-gray md:w-72 transition-all duration-300 ${
-          openSidebar ? "w-72" : "w-16"
-        }`}
+        className={`h-full fixed inset-y-0 z-[80] bg-gray md:w-72 sm:left-0  transition-all duration-300 ${
+          openSidebar ? "sm:w-72" : "sm:w-16"
+        } ${openSidebar ? "left-0 w-72" : "-left-full"}`}
       >
         <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
       </aside>
-      <main className="md:pl-72 pl-14  min-h-full w-full bg-grayFade">
+      <main className="md:pl-72 sm:pl-16  min-h-full w-full bg-grayFade">
         <Navbar setOpenSidebar={setOpenSidebar} />
         {children}
       </main>
