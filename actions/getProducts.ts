5a3e042ea131d167e237525a06c9b16f5165388f
Qreamville <Oyeleyebalikis@yahoo.com>
2023-10-08 16:33:50 +0100

    build: fixes

--- actions/getProducts.ts
+++ actions/getProducts.ts
@@ -1,12 +1,18 @@
 import prisma from "@/lib/prismadb";
-import { auth } from "@clerk/nextjs";
+// import { auth } from "@clerk/nextjs";
 
 export const getProducts = async () => {
   try {
-    const { userId } = auth();
-    if (!userId) return [];
+    // const { userId } = auth();
+    // if (!userId) return [];
 
-    const products = await prisma.product.findMany();
+    const products = await prisma.product.findMany({
+      where: {
+        sellerId: {
+          equals: "",
+        },
+      },
+    });
 
     return products;
   } catch (error: any) {
