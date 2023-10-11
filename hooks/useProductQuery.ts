42e8e0e9929dc954054473066ef75587961b7e2e
Qreamville <Oyeleyebalikis@yahoo.com>
2023-10-11 15:32:03 +0100

    delete: remove hook

--- hooks/useProductQuery.ts
+++
@@ -1,9 +0,0 @@
-import { QueryStore } from "@/types/general";
-import { create } from "zustand";
-
-const useQuery = create<QueryStore>((set) => ({
-  query: "",
-  setQuery: (query) => set({ query: query }),
-}));
-
-export default useQuery;
