49c34ca42a765cb2b41580d8c23d26d8ceb4d59a
Qreamville <Oyeleyebalikis@yahoo.com>
2023-11-10 09:24:26 +0100

    feat: update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className="flex justify-center items-center h-[80svh] flex-col gap-3">
       <h1 className="text-7xl font-bold">404</h1>
-      <p>We couldn't find that page</p>
+      <p>We {"couldn't"} find that page</p>
       <Link href="/" className="px-4 bg-gray py-2 text-white rounded-md">
         Back to home
       </Link>
